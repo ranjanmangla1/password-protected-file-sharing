0e734c879f770dfbcf515302ce9ea3c3be965d37
ranjanmangla1 <ranjanmangla1@gmail.com>
2023-06-12 23:10:11 +0530

    added file password logic

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 require("dotenv").config()
 
+const multer = require("multer")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const File = require("./models/file")
+
+// const req = require("express/lib/request")
+
 const express = require("express")
 const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect(process.env.DATABASE_URL)
+
+const upload = multer({ dest: "uploads"})
 
 // configuring our view engine, ejs
 app.set("view engine", "ejs")
@@ -11,7 +24,56 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+app.post("/upload", upload.single("file"), async (req, res) => {
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname
+    }
 
-console.log(`server running at https://localhost:${process.env.PORT}`)
+    if(req.body.password != null && req.body.password != "") {
+        fileData.password = await bcrypt.hash(req.body.password, 10)
+    }
+
+    const file = await File.create(fileData)
+
+    res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
+}) 
+
+app.route("/file/:id").get(handleDownload).post(handleDownload)
+
+async function handleDownload(req, res) {
+    const file = await File.findById(req.params.id)
+
+    if(file.password != null) {
+        if(req.body.password == null) {
+            res.render("password")
+            return
+        }
+
+        if (!(await bcrypt.compare(req.body.password, file.password))) {
+            res.render("password", { error: true })
+            return
+        }
+    }
+
+    file.downloadCount++
+    await file.save()
+    console.log(file.downloadCount)
+  
+    res.download(file.path, file.originalName)
+}
+
+
+// app.get("/file/:id", async (req, res) => {
+//     const file = await File.findById(req.params.id)
+
+//     file.downlaodCount++;
+//     await file.save();
+
+//     console.log(file.downlaodCount);
+
+//     res.download(file.path, file.originalName)
+// })
+
 // it will be listening on port 3000
 app.listen(process.env.PORT)
